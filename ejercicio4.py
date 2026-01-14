c4233ab5fd8414aade7fa1a0a5ee9f1078766d3b
KendallAP <kendallcek@gmail.com>
2026-01-13 19:36:56 -0600

    avance

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,5 +1,17 @@
-# realizamos la operacion usando potencias
+# realizamos la operacion usando operaciones aritmeticas 
 
-resultado = 2 ** 2 
+suma1 = 3 + 2
 
-print("El resultado de 2 elevado a la 2 es: " + str(resultado))
+print("El resultado de la suma es: " + str(suma1))
+
+multiplicacion = 2 * 5
+
+print("El resultado de la multiplicacion es: " + str(multiplicacion))
+
+division = suma1 / multiplicacion
+
+print("El resultado de la division es: " + str(division))
+
+potencia = 0.5 ** 2
+
+print("El resultado de 0.5 elevado a la 2 es: " + str(potencia))
