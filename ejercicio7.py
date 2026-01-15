2f44eb7f4ed764aadba5f910e072edbf3ff9e058
KendallAP <kendallcek@gmail.com>
2026-01-15 00:36:36 -0600

    avance

---
+++ ejercicio7.py
@@ -0,0 +1,17 @@
+num1 = int(input("Ingrese el primer numero: "))
+
+num2 = int(input("Ingrese el segundo numero: "))
+
+print("El primer numero es:", num1)
+
+print("El segundo numero es:", num2)    
+
+division = num1 / num2
+
+print("La division de", num1, "entre", num2, "es:", division)   
+
+modulo = num1 % num2    
+
+print("El modulo de", num1, "entre", num2, "es:", modulo)   
+
+
