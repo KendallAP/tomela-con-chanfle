8b51a6345b461d95e0a7e9d4a9fdc59d5351b58d
KendallAP <kendallcek@gmail.com>
2026-01-15 00:25:34 -0600

    avance

---
+++ ejercicio6.py
@@ -0,0 +1,8 @@
+entero = int(input("Ingrese un numero entero: "))
+
+print("El numero ingresado es:", entero)
+
+suma= entero *(entero + 1) // 2
+
+print("La suma de los primeros", entero, "numeros enteros es:", suma)   
+
