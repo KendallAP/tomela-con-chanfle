dc534461a9139f036618e441f89c6faa5ef0e23b
KendallAP <kendallcek@gmail.com>
2026-01-15 00:14:33 -0600

    avance

---
+++ ejercicio5.py
@@ -0,0 +1,7 @@
+horas = input("Ingrese las horas trabajadas: ")
+
+dinero = input("Ingrese el dinero ganado por hora: ")   
+
+pago = float(horas) * float(dinero) 
+
+print("El pago total es: " + str(pago))
