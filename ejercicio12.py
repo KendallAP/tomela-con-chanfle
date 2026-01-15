ad26490b79ad19c79e930fbecd2dd8b6e95fe07b
KendallAP <kendallcek@gmail.com>
2026-01-15 01:50:00 -0600

    avance

---
+++ ejercicio12.py
@@ -0,0 +1,11 @@
+nombre_producto = input("Ingresa el nombre del producto: ")
+
+precio_producto = float(input("Ingresa el precio del producto: "))
+
+cantidad_producto = int(input("Ingresa la cantidad del producto: "))
+
+total_pagar = precio_producto * cantidad_producto
+
+print(nombre_producto, "tiene un precio de", precio_producto, "el cliente quiere adquirir un total de", cantidad_producto, "y el precio a pagar es", total_pagar)
+
+
