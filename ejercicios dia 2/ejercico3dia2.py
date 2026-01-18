61c40ed6af7e4d24a5a1742236b7208bf05b768d
KendallAP <kendallcek@gmail.com>
2026-01-18 00:52:47 -0600

    se realiza el ejercicio 3 completo

---
+++ ejercicios dia 2/ejercico3dia2.py
@@ -0,0 +1,10 @@
+#le pedimos al usuario que eliga un numero entre uno y tres
+num = int(input("Elige un número entre uno y tres: "))
+
+if num == 1:
+    print("Uno!")
+elif num == 2:
+    print("Dos!")
+elif num == 3:
+    print("Tres!")
+
