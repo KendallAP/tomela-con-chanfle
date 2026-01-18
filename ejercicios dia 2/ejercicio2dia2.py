4b8c4289bc9035d07be8d9513f90e0ab3193b16b
KendallAP <kendallcek@gmail.com>
2026-01-18 00:41:28 -0600

    se realiza el ejercicio 2

---
+++ ejercicios dia 2/ejercicio2dia2.py
@@ -0,0 +1,9 @@
+#le pedimos al usuario que ingrese dos numeros 
+
+num1 = float(input("Ingresa el primer número: "))
+
+num2 = float(input("Ingresa el segundo número: "))
+
+#comparamos los numeros
+
+print("¿El primer número es menor que el segundo?", num1 < num2)
