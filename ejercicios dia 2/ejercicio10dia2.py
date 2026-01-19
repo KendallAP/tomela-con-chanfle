7d99bcfa1693e995f3b6e4297f145404b9305983
KendallAP <kendallcek@gmail.com>
2026-01-18 23:37:45 -0600

    dudas sobre ejercicio 10

---
+++ ejercicios dia 2/ejercicio10dia2.py
@@ -0,0 +1,10 @@
+#le pedimos al usuario que ponga dos numeros enteros
+
+numero1 = int(input("Ingresa el primer número entero: "))
+
+numero2 = int(input("Ingresa el segundo número entero: "))
+
+if numero1 < numero2:
+    for i in range (numero1, numero2 + 1):
+        if i % 2 == 0:
+            print(i) 
