376d2588b8e23e839db13a677c4742b1d2f8b2ad
KendallAP <kendallcek@gmail.com>
2026-01-22 00:16:33 -0600

    avance ejercicio 4

---
+++ ejerciciosdia3/ejercicio4.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+public class ejercicio4 {
+    public static double CelciusAFarenheit(double celcius) {
+        return (celcius * 9/5) + 32;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingresa los grados Celsius: ");
+    }
+}
